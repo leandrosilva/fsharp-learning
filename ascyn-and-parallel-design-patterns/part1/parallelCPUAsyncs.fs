36e1cc8fcad81ca56edd26f1c4541563b1c33df6
Leandro Silva <leandrodoze@gmail.com>
2010-08-14 23:26:11 -0300

    fix

--- ascyn-and-parallel-design-patterns/part1/parallelCPUAsyncs.fs
+++ ascyn-and-parallel-design-patterns/part1/parallelCPUAsyncs.fs
@@ -6,7 +6,7 @@ let rec fib x =
 // (2) These are composed in parallel using the fork-join combinator Async.Parallel
 //
 // (3) In this case the composition is executed using Async.RunSynchronously, which
-//     starts an instance of the async and synchronously waits for the overall result. 
+//     starts an instance of the async and synchronously waits for the overall result
 let fibs =
   Async.Parallel [for i in 0..40 -> async { return fib(i) }]
   |> Async.RunSynchronously
